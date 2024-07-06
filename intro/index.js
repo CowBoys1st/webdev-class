5f6c57619da184dc4faf12969e337a01a351ca85
CowBoys1st <draharjo9@gmail.com>
2024-07-06 13:42:43 +0700

    intro programming

---
+++ intro/index.js
@@ -0,0 +1,21 @@
+// Sigle line
+// Menampilkan teks pada console / terminal
+console.log("menampilkan komentar")
+console.log("menampilkan komentar")
+console.log("menampilkan komentar")
+
+// Shortcut Comment : CTRL /
+// Menampilkan hasil penjumlahan dari dua angka
+// console.log(2 + 4)
+
+// Multi line
+
+/* 
+    Jika memiliki komentar yang membutuhkan
+    lebih dari satu baris, gunakanlah cara ini
+
+    karena akan lebih mudah dibanding harus menggunakan
+    double slash // pada tiap barisnya
+
+
+*/
